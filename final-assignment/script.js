718430e6b3ab75630c87e6c6f201611597a41aed
dylanvans <dylanvansprang@gmail.com>
2017-03-29 15:41:50 +0200

    fallback for indexof()

--- final-assignment/script.js
+++ final-assignment/script.js
@@ -2,7 +2,20 @@
 	var searchInputEl = document.getElementsByClassName('search-input')[0];
 	var contactEl = document.getElementsByClassName('contact-item');
 	var mainContactEl = document.getElementsByClassName('contact-main-info');
+	
 
+	// source: https://stackoverflow.com/questions/2790001/fixing-javascript-array-functions-in-internet-explorer-indexof-foreach-etc
+	if (!('indexOf' in Array.prototype)) {
+	    Array.prototype.indexOf= function(find, i /*opt*/) {
+	        if (i===undefined) i= 0;
+	        if (i<0) i+= this.length;
+	        if (i<0) i= 0;
+	        for (var n= this.length; i<n; i++)
+	            if (i in this && this[i]===find)
+	                return i;
+	        return -1;
+	    };
+	}
 
 	if (document.addEventListener) {
 	    for (var i = 0; i < mainContactEl.length; i++) {
@@ -21,6 +34,7 @@
 
 
 	function searchContacts() {
+		console.log('searchContacts');
 		var filterValue = searchInputEl.value.toUpperCase();
 
 	    for (i = 0; i < contactEl.length; i++) {
@@ -34,6 +48,7 @@
 	}
 
 	function toggleAccordion() {
+		console.log('toggleAccordion');
         for (i = 0; i < contactEl.length; i++) {
             contactEl[i].className = 'contact-item  acc-inactive';
         }
