98623e2b3448ce2c55557567b298d274918e76c4
dylanvans <dylanvansprang@gmail.com>
2017-03-29 15:18:10 +0200

    queryselector -> getElementsByClassName

--- final-assignment/script.js
+++ final-assignment/script.js
@@ -1,7 +1,7 @@
 (function () {
-	var searchInputEl = document.querySelector('.search-input');
-	var contactEl = document.querySelectorAll('li.contact-item');
-	var mainContactEl = document.querySelectorAll('.contact-main-info');
+	var searchInputEl = document.getElementbyClassName('.search-input');
+	var contactEl = document.getElementsbyClassName('.contact-item');
+	var mainContactEl = document.getElementsbyClassName('.contact-main-info');
 
 	for (var i = 0; i < mainContactEl.length; i++) {
 		mainContactEl[i].addEventListener('click', toggleAccordion);
@@ -13,7 +13,7 @@
 		var filterValue = searchInputEl.value.toUpperCase();
 
 	    for (i = 0; i < contactEl.length; i++) {
-	        var name = contactEl[i].querySelector('.contact-name');
+	        var name = contactEl[i].getElementbyClassName('.contact-name');
 	        if (name.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
 	            contactEl[i].style.display = '';
 	        } else {
